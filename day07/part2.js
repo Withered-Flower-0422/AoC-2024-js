d95e86b2bf6f1d197de5b6439b7b18f8efc2965c
枯萎の花 <142531691+Withered-Flower-0422@users.noreply.github.com>
2024-12-07 16:11:50 +0800

    Add files via upload

---
+++ day07/part2.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+
+const choice = 1
+const file = ['example.txt', 'puzzle.txt'][choice]
+const data = fs.readFileSync(path.join(__dirname, file), 'utf8')
+
+const equations = data.split('\r\n')
+
+const canHoldTrue = e => {
+    const [[target], nums] = e.split(': ').map(s => s.split(' ').map(Number))
+    const cache = {}
+    const _canHoldTrue = (n, t) => {
+        const tag = `${n.toString()},${t}`
+        if (tag in cache) return cache[e]
+        if (n.length === 1) return n[0] === t
+        const n0 = n.slice(2)
+        const n1 = [n[0] + n[1], ...n0]
+        const n2 = [n[0] * n[1], ...n0]
+        const n3 = [+`${n[0]}${n[1]}`, ...n0]
+        return cache[tag] = _canHoldTrue(n1, t) || _canHoldTrue(n2, t) || _canHoldTrue(n3, t)
+    }
+    return _canHoldTrue(nums, target)
+}
+
+let cnt = 0
+equations.forEach(e => {
+    if (canHoldTrue(e)) cnt += +e.split(': ')[0]
+})
+
+console.log(cnt)
