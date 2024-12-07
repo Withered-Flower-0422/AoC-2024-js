4efb4f70c9fc1d86cee1381d13ce8acf4df3f6cb
枯萎の花 <142531691+Withered-Flower-0422@users.noreply.github.com>
2024-12-07 16:21:19 +0800

    Update part1.js

--- day07/part1.js
+++ day07/part1.js
@@ -9,15 +9,12 @@ const equations = data.split('\r\n')
 
 const canHoldTrue = e => {
     const [[target], nums] = e.split(': ').map(s => s.split(' ').map(Number))
-    const cache = {}
     const _canHoldTrue = (n, t) => {
-        const tag = `${n.toString()},${t}`
-        if (tag in cache) return cache[e]
         if (n.length === 1) return n[0] === t
         const n0 = n.slice(2)
         const n1 = [n[0] + n[1], ...n0]
         const n2 = [n[0] * n[1], ...n0]
-        return cache[tag] = _canHoldTrue(n1, t) || _canHoldTrue(n2, t)
+        return _canHoldTrue(n1, t) || _canHoldTrue(n2, t)
     }
     return _canHoldTrue(nums, target)
 }
